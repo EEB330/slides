a4a70ad95d90ef845fd4ae5b024aa012bc4ae678
Diogo Melo <diogro@gmail.com>
2023-07-12 13:49:11 -0400

    libs and figs

---
+++ build_site.R
@@ -0,0 +1,6 @@
+# pak::pkg_install(c("xaringan", "xaringanExtra", "leaflet", 'knitr'))
+# xaringan::summon_remark()
+
+library(rmarkdown)
+render("00_Why_Programming.Rmd")
+render("01_Computer_programming.Rmd")
