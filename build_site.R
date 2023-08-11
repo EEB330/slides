49eb60f720ece99b6ab69e6177b33cbf47c48c52
Diogo Melo <diogro@gmail.com>
2023-08-11 10:35:12 -0400

    new stuff

--- build_site.R
+++ build_site.R
@@ -2,7 +2,9 @@
 # xaringan::summon_remark()
 
 library(rmarkdown)
+# Week 1
 render("00_Why_Programming.Rmd")
 render("01_Computer_programming.Rmd")
+# Week 2
 render("02_BasicR.Rmd")
 render("03_BasicGit.Rmd")
