5feb86733ecb7e500f0c8e97e97ff32cdf970a64
Diogo Melo <diogro@gmail.com>
2023-09-01 11:57:37 -0400

    random build crap

--- build_site.R
+++ build_site.R
@@ -1,16 +1,16 @@
-pak::pkg_install(c("xaringan", "xaringanExtra", "leaflet", 'knitr', "stringi", "cowplot", "readr", "data.table", "curl"))
-xaringan::summon_remark()
+# pak::pkg_install(c("xaringan", "xaringanExtra", "leaflet", 'knitr', "stringi", "cowplot", "readr", "data.table", "curl"))
+# xaringan::summon_remark()
 
 library(rmarkdown)
 ## Week 1
 render("00_Why_Programming.Rmd")
 render("01_Computer_programming.Rmd")
 # ## Week 2
-render("02_BasicR.Rmd")
-render("03_BasicGit.Rmd")
-# ## Week 3
-render("04_ControlFlow.Rmd")
-render("05_Functions.Rmd")
-# ## Week 4
-render("06_DataIO.Rmd")
+# render("02_BasicR.Rmd")
+# render("03_BasicGit.Rmd")
+# # ## Week 3
+# render("04_ControlFlow.Rmd")
+# render("05_Functions.Rmd")
+# # ## Week 4
+# render("06_DataIO.Rmd")
 
