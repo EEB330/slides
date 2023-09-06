9a5b84dc60cbf586068028ac8ebfb513363fd7ff
Diogo Melo <diogro@gmail.com>
2023-09-06 09:39:59 -0400

    git changes

--- build_site.R
+++ build_site.R
@@ -7,7 +7,7 @@ render("00_Why_Programming.Rmd")
 render("01_Computer_programming.Rmd")
 # ## Week 2
 render("02_BasicR.Rmd")
-# render("03_BasicGit.Rmd")
+render("03_BasicGit.Rmd")
 # # ## Week 3
 # render("04_ControlFlow.Rmd")
 # render("05_Functions.Rmd")
