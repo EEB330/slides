a693d54e9a35298a51059e34418d1bf0cd18644e
Diogo Melo <diogro@gmail.com>
2023-09-27 16:44:13 -0400

    data wrangling

--- build_site.R
+++ build_site.R
@@ -13,7 +13,7 @@ library(rmarkdown)
 # render("05_Functions.Rmd")
 # # ## Week 4
 #render("06_DataIO.Rmd")
-render("07_RegularExpressions.Rmd")
+#render("07_RegularExpressions.Rmd")
 # # ## Week 4
 render("08_DataWrangling1.Rmd")
 render("09_DataWrangling2.Rmd")
