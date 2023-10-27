7c3d4426f6b40ed29bc6f4198a89c1c0714aeee4
Diogo Melo <diogro@gmail.com>
2023-10-27 11:10:25 -0400

    for realz

--- build_site.R
+++ build_site.R
@@ -6,28 +6,30 @@ library(MASS)
 library(plyr)
 library(tidyverse)
 library(covidcast)
+library(cowplot)
+library(patchwork)
 ## Week 1
 #render("00_Why_Programming.Rmd")
 #render("01_Computer_programming.Rmd")
-# ## Week 2
+## Week 2
 #render("02_BasicR.Rmd")
 #render("03_BasicGit.Rmd")
-# # ## Week 3
+## Week 3
 # render("04_ControlFlow.Rmd")
 # render("05_Functions.Rmd")
-# # ## Week 4
+## Week 4
 #render("06_DataIO.Rmd")
 #render("07_RegularExpressions.Rmd")
-# # ## Week 4
+## Week 4
 #render("08_DataWrangling1.Rmd")
 #render("09_DataWrangling2.Rmd")
-# # ## Week 5
+## Week 5
 #render("10_SplitApplyCombine.Rmd")
 
-# # ## Week 6
+## Week 6
 # render("12_GrammarOfGraphics.Rmd")
 
-# Week 7
+## Week 7
 render("13_GrammarOfGraphics2.Rmd")
 render("14_MakingPlots.Rmd")
 
